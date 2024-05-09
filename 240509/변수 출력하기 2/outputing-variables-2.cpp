febb4d00710621fe84ec5ae92e67f58decf8f360
sonminseung <105152528+sonminseung@users.noreply.github.com>
2024-05-09 20:30:15 +0900

    Title: 변수 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240509/변수 출력하기 2/outputing-variables-2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    int a= 3;
+    string b ="C";
+    cout << b << "!.....!" << a;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
