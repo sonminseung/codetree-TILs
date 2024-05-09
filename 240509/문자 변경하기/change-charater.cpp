1e2588169fdf4bfd3d31fd7fb11407ae38927f2f
sonminseung <105152528+sonminseung@users.noreply.github.com>
2024-05-09 21:03:00 +0900

    Title: 문자 변경하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240509/문자 변경하기/change-charater.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string a = "C";
+    a = "T";
+    cout << a;
+    // 여기에 코드를 작성해주세요. 변수 a에 C 를 넣고, 그 후 다시 a에 T를 넣어 줍니다
+    return 0;
+}
