5cdebd968123635f2c01a06ee387368640f499e5
sonminseung <105152528+sonminseung@users.noreply.github.com>
2024-05-09 20:58:14 +0900

    Title: 두 실수의 곱, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240509/두 실수의 곱/the-product-of-two-real-numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << fixed;
+    double a = 5.26;
+    double b = 8.27;
+    cout.precision(3);
+    cout << a * b;
+    // 여기에 코드를 작성해주세요. 5.26, 8.27
+    return 0;
+}
