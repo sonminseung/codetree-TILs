369406b8d466aace8f7ce2e4062e0196078ef2df
sonminseung <105152528+sonminseung@users.noreply.github.com>
2024-05-11 10:43:01 +0900

    Title: 간단한 사칙연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240511/간단한 사칙연산/simple-arithmetic-operation.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    scanf("%d %d",&a,&b);
+    printf("%d\n",a+b);
+    printf("%d\n",a-b);
+    printf("%d\n",a/b);
+    printf("%d",a%b);
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
