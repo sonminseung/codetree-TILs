3373a8252c8b61882d9f09530480d1d45fea9555
sonminseung <105152528+sonminseung@users.noreply.github.com>
2024-05-11 10:21:05 +0900

    Title: 실수 입력받아 계산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240511/실수 입력받아 계산/calculation-by-inputting-a-real-number.c
+++ 240511/실수 입력받아 계산/calculation-by-inputting-a-real-number.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    double a = 5.26;
-    double b = 3.14;
+    double a ;
+    double b ;
+    scanf("%lf %lf", &a , &b);
     printf("%.2lf",a+b);
     // 여기에 코드를 작성해주세요.
     return 0;
