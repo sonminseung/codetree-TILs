7dd535bc8100d0152d56191a80d4c9afda9e96b6
sonminseung <105152528+sonminseung@users.noreply.github.com>
2024-06-20 14:50:56 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240620/4번의 크기 비교/4-time-comparison.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a,b,c,d,e;
+    scanf("%d\n",&a);
+    scanf("%d %d %d %d",&b,&c,&d,&e);
+    printf("%d\n", a>b);
+    printf("%d\n",  a>c);
+    printf("%d\n",  a>d);
+    printf("%d\n", a>e);
+    return 0;
+}
