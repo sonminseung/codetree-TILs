cf6ab14959ff8e72192b94ef5c95cd318f8a424e
sonminseung <105152528+sonminseung@users.noreply.github.com>
2024-06-20 15:12:08 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240620/두 사람/two-person.c
+++ 240620/두 사람/two-person.c
@@ -5,7 +5,7 @@ int main() {
     char a1,b1;
     scanf("%d %c\n", &a,&a1);
     scanf("%d %c", &b,&b1);
-    if ((a>19 && a1=='M') || (b>19 && b1=='W'))
+    if ((a>=19 && a1=='M') || (b>=19 && b1=='W'))
     printf("1");
     else
     printf("0");
