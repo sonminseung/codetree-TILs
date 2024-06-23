af180a9d1270f1fe723c1464e623692b852086f1
sonminseung <105152528+sonminseung@users.noreply.github.com>
2024-06-23 17:07:02 +0900

    Title: 1부터의 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240623/1부터의 합/sum-from-the-one.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    int sum=0;
+    scanf("%d", &n);
+    for(int i=1; i<=n; i++)
+    {
+        if(sum>=i)
+        {
+            break;
+        }
+        else
+        {
+            sum+=i;
+        }
+    }
+    printf("%d", sum);
+    return 0;
+}
