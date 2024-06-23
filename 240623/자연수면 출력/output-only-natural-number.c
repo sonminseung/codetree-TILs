50934559146feb9821749be8efc1a4adc8f687e0
sonminseung <105152528+sonminseung@users.noreply.github.com>
2024-06-23 15:54:03 +0900

    Title: 자연수면 출력, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240623/자연수면 출력/output-only-natural-number.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    scanf("%d %d", &a,&b);
+    if(a>0)
+    {
+        for(int i=0; i<b; i++)
+        {
+            printf("%d",a);
+        }
+    }
+    if(a<=0)
+    {
+        printf("0");
+    }
+
+
+    return 0;
+}
