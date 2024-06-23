5112c0e68e3fa96dfe52f596a0c35ee70a23ef0b
sonminseung <105152528+sonminseung@users.noreply.github.com>
2024-06-23 16:04:38 +0900

    Title: 홀수만 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240623/홀수만 출력하기/print-only-odd-numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    int a;
+    scanf("%d", &n);
+    for(int i=1; i<=5; i++)
+    {
+        scanf("%d\n",&a);
+        if((a%2==1) && (a%3==00))
+        {
+            printf("%d\n",a);
+        }
+    }
+    return 0;
+}
