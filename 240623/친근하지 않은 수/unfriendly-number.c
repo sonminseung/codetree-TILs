05a7780289ccd2faf128f9234801bc71c3de4ec4
sonminseung <105152528+sonminseung@users.noreply.github.com>
2024-06-23 16:50:59 +0900

    Title: 친근하지 않은 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240623/친근하지 않은 수/unfriendly-number.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    int count=0;
+    scanf("%d", &n);
+    for(int i=1; i<=n; i++)
+    {
+        if ((i%2==0)||(i%3==0)||(i%5==0))
+        {
+        continue;
+        }
+        else
+        {
+        count+=1;
+        }
+    }
+    printf("%d", count);
+    return 0;
+}
