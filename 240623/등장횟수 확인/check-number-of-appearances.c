c5987a5c99fb1c498e4ebfa605e63afaccf80d47
sonminseung <105152528+sonminseung@users.noreply.github.com>
2024-06-23 16:14:30 +0900

    Title: 등장횟수 확인, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240623/등장횟수 확인/check-number-of-appearances.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    int cnt=0;
+    for(int i=1; i<=5; i++)
+    {
+        scanf("%d",&n);
+        if(n%2==0)
+        {
+            cnt++;
+        }
+    }
+    printf("%d", cnt);
+    return 0;
+}
